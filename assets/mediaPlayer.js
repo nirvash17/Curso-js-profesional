3a9ee958b70065dced693ccf3af9a1384388ffd4
nirvash <josee_fw_900@hotmail.cl>
2022-02-05 16:15:08 -0300

    Empiezo una rama para trabajar con class en lugar de protorype, que es lo que se usa desde ES6

---
+++ assets/mediaPlayer.js
@@ -0,0 +1,50 @@
+class mediaPlayer{
+      //Tengo que intentar hcaer esto con clases, mejor notacion
+    constructor({video, plugins}){
+        this.media = config.el;
+        this.plugins = config.plugins || [];
+        this._initPlugins();
+    }
+
+
+mediaPlayer.prototype._initPlugins = function(){
+
+    this.plugins.forEach( plugin => {
+        plugin.run(this);  //Esta es una instancia de un plugin, que es un objeto,se deben usar metodos => se crea el objeto plugins en autoplay
+    })                      //Se envia la instancia de media player para que pueda usaral el plugin
+
+}
+    
+mediaPlayer.prototype.play = function(){
+this.media.play();
+}
+
+mediaPlayer.prototype.pause = function(){
+this.media.pause();
+}
+
+mediaPlayer.prototype.play_pause = function(){ //Metodo que controla el play y pausa con el boton play
+console.log(`paused: ${this.media.paused}`);
+
+this.media.paused ? this.play() : this.pause(); 
+}
+
+// mediaPlayer.prototype.mute = function(){ Solucion con 3 metodos, una para mutear, para desmutear y uno que cambia entre ambos con un boton
+//     this.media.muted = true;
+// }
+
+// mediaPlayer.prototype.unmute = function(){
+//     this.media.muted = false;
+// }
+
+// mediaPlayer.prototype.mute_unmute = function(){  //Controla el muteo con el boton de muteo
+//     console.log(`muted: ${this.media.muted}`);
+    
+//     this.media.muted ? this.unmute() : this.mute(); 
+// }
+
+mediaPlayer.prototype.toggleMute = function(){ //Esta solucion mas corta permite cambiar el muteo con un boton y ademas al cargar la pagina
+    this.media.muted = !this.media.muted //Muy bonito
+}
+
+export default mediaPlayer; 
