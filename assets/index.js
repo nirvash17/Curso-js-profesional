0de247e42140105292e2c9123ffe7ced10f23187
nirvash <josee_fw_900@hotmail.cl>
2022-02-05 17:30:18 -0300

    Ejercicio y apuntes para entender THIS

--- assets/index.js
+++ assets/index.js
@@ -6,11 +6,7 @@ const buttonPlay = document.querySelector('.play');
 const buttonMute = document.querySelector('.mute');
 
 
-<<<<<<< HEAD
 const player = new mediaPlayer({video, plugins: [new autoPlay(video)] });
-=======
-const player = new mediaPlayer({el: video, plugins: [new autoPlay()] });
->>>>>>> e37e659bb5ae045c0f961c2068540b4b5f8ee5ee
 
 buttonPlay.onclick = () => player.play_pause();
 buttonMute.onclick = () => player.toggleMute();
--- assets/plugins/autoPlay.js
+++ assets/plugins/autoPlay.js
@@ -1,9 +1,7 @@
-<<<<<<< HEAD
 class autoPlay{
 
     constructor(video){
         this.player = video;
-        console.log('xd ', + video)
 
     }
 
@@ -13,15 +11,6 @@ class autoPlay{
         this.player.play();    
     }
 }
-=======
-function autoPlay(){}
 
-autoPlay.prototype.run = function(player){
-   // player.defaultMuted = true;
-    console.log(player)
-    player.toggleMute(); //El plugin juega con los metodos del reproductor
-    player.play();
-}
 
->>>>>>> e37e659bb5ae045c0f961c2068540b4b5f8ee5ee
 export default autoPlay;
