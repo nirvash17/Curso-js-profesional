befeab09aad3ccb0a2c4fa908ded9ae4beb8d9de
nirvash <josee_fw_900@hotmail.cl>
2022-02-18 17:13:55 -0300

    Solucione el uso del parcel-bundler y agregue el cache del parcel al gitignore

--- assets/index.js
+++ assets/index.js
@@ -13,11 +13,14 @@ buttonPlay.onclick = () => player.play_pause();
 buttonMute.onclick = () => player.toggleMute();
 
 
+
+
 /* Se verifica que el browser tenga serviceWorker */
 if ('serviceWorker' in navigator){
-    /*se usa el metodo .register() para relacionar la URL del scrip dado (/sw.js) con un ambito (el repo de la pagina, la ruta inicial de la pagina)*/
-    navigator.serviceWorker.register('/sw.js')//Retorna una promesa a resolver cuando se haya hecho el registro
+    //se usa el metodo .register() para relacionar la URL del scrip dado (/sw.js) con un ambito (el repo de la pagina, la ruta inicial de la pagina)
+    navigator.serviceWorker.register(new URL('/sw.js', import.meta.url))//Retorna una promesa a resolver cuando se haya hecho el registro
     .catch(error => {
         console.log(error.message)  //Por si sale mal se le asigna un catch
     })
 }
+ 
