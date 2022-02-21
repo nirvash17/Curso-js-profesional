58970bdf1ea46713d0bf05c6c7b456b59a2dda45
nirvash <josee_fw_900@hotmail.cl>
2022-02-20 22:10:30 -0300

    hice el tsconfig.json y le puse es2020 parap poder usar bien ts, ahora funciona el archiv index.ts

---
+++ assets/index.ts
@@ -0,0 +1,29 @@
+import mediaPlayer from "./mediaPlayer";
+import autoPlay from './plugins/autoPlay'; //Lo escribo antes de que existe, para arreglar sobre la marcha
+import autoPause from "./plugins/autoPause";
+
+const video :HTMLElement= document.querySelector("video");
+const buttonPlay:HTMLElement  = document.querySelector('.play');
+const buttonMute:HTMLElement = document.querySelector('.mute');
+
+
+const player = new mediaPlayer({
+    el: video,
+    plugins: [new autoPlay(), new autoPause()],
+  });
+  
+buttonPlay.onclick = () => player.play_pause();
+buttonMute.onclick = () => player.toggleMute();
+
+
+
+
+/* Se verifica que el browser tenga serviceWorker */
+if ('serviceWorker' in navigator){
+    //se usa el metodo .register() para relacionar la URL del scrip dado (/sw.js) con un ambito (el repo de la pagina, la ruta inicial de la pagina)
+    navigator.serviceWorker.register(new URL('/sw.js', import.meta.url))//Retorna una promesa a resolver cuando se haya hecho el registro
+    .catch(error => {
+        console.log(error.message)  //Por si sale mal se le asigna un catch
+    })
+}
+ 
